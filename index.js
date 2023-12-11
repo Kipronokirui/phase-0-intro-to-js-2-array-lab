c47fdf2b2ee127460e847f0492e2e05e5146bbc2
Kipronokirui <victorkipronokirui2@gmail.com>
2023-12-11 12:29:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function handleCats(){
+    console.log(`Initial cats length: ${cats.length}`)
+    console.log(`Old cats: ${cats}`)
+    cats.push("Milo", "Otis", "Garfield")
+    console.log(`New cats length: ${cats.length}`)
+    console.log(`New cats: ${cats}`)
+}
+handleCats()
